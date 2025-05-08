11f7f74cab848fe61fc22493999c064052ef580b
Jayla <157384684+Jayla194@users.noreply.github.com>
2025-05-08 08:04:00 +0100

    First Commit
    Uploaded the progress made so far as a test

---
+++ Section 2 - Website/js/reviewData.js
@@ -0,0 +1,22 @@
+
+//User_Name isn't required but is in use for the 
+const reviews = [
+    {
+        "Review_ID":1,
+        "Location_ID":1,
+        "User_ID":1,
+        "User_Name":"Jayla Green",
+        "Rating":3,
+        "Text":"Very Scenic location. I felt like I was fully immersed in the book.",
+        "Rating_Likes":4
+    },
+    {
+        "Review_ID":2,
+        "Location_ID":13,
+        "User_ID":1,
+        "User_Name":"Jayla Green",
+        "Rating":4,
+        "Text":"Tickets were rather expensive but they were totally worth it. Had an amazing time!",
+        "Rating_Likes":14
+    }
+]
